4ba97bd5e341c8f6302257ab2b5786620a513133
itsDipsy <elliot.collinszcv@gmail.com>
2024-01-05 22:10:27 +0100

    uppdate

--- .godot/mono/temp/obj/Debug/Test1.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Test1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d08906851ea971f314521ffda7728cec693230a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
